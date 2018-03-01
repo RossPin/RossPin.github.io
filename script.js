f6667ec132e7f94ce9c0b8f7ab49f5710fd6f51f
RossPin <dorpetz@hotmail.com>
2018-03-01 17:16:05 +1300

    Update javascript navbar to enable future parent lists

--- script.js
+++ script.js
@@ -9,17 +9,28 @@ function populateNav (){
   if (document.getElementsByTagName('nav')[0].classList.contains('indexPage')){
     folder = 'blog/'
   }
-  var cultural = document.getElementById('culturalBlogList')
-  var technical = document.getElementById('technicalBlogList')
-  addListItem(folder, 'c1-reflection-blog.html', 'Reflection', cultural)
-  addListItem(folder, 'c1-time-and-habits-blog.html', 'Time and Habits', cultural)
-  addListItem(folder, 'c2-emotional-intelligence.html', 'Emotional Intelligence', cultural)
-  addListItem(folder, 'c3-meditation-process.html', 'Meditation Process', cultural)
-  addListItem(folder, 'c4-daily-mindfulness.html', 'Daily Mindfulnes', cultural)
-  addListItem(folder, 't2-html-css-dom-p1.html', 'HTML, CSS and DOM Part 1', technical)
-  addListItem(folder, 't2-html-css-dom-p2.html', 'HTML, CSS and DOM Part Part 2', technical)
-  addListItem(folder, 't3-design-to-web-blog.html', 'Design to Web', technical)
-  addListItem(folder, 't4-javascript-basics.html', 'Technical Blog: JavaScript Basics', technical)
+  var navList = document.getElementById('navlist') 
+  populateCultural(folder, navList)
+  populateTech(folder, navList)
+}
+
+function populateCultural(folder, list) {  
+  list.innerHTML += '<li>Cultural Blogs<ul id="culturalBlogList"></ul></li>';
+  var culturalList = document.getElementById('culturalBlogList')
+  addListItem(folder, 'c1-reflection-blog.html', 'Reflection', culturalList);
+  addListItem(folder, 'c1-time-and-habits-blog.html', 'Time and Habits', culturalList);
+  addListItem(folder, 'c2-emotional-intelligence.html', 'Emotional Intelligence', culturalList);
+  addListItem(folder, 'c3-meditation-process.html', 'Meditation Process', culturalList);
+  addListItem(folder, 'c4-daily-mindfulness.html', 'Daily Mindfulnes', culturalList);  
+}
+
+function populateTech(folder, list) {  
+  list.innerHTML += '<li>Technical Blogs<ul id="technicalBlogList"></ul></li>' 
+  var technicalList = document.getElementById('technicalBlogList')
+  addListItem(folder, 't2-html-css-dom-p1.html', 'HTML, CSS and DOM Part 1', technicalList)
+  addListItem(folder, 't2-html-css-dom-p2.html', 'HTML, CSS and DOM Part Part 2', technicalList)
+  addListItem(folder, 't3-design-to-web-blog.html', 'Design to Web', technicalList)
+  addListItem(folder, 't4-javascript-basics.html', 'Technical Blog: JavaScript Basics', technicalList)  
 }
 
 function addListItem (folder, file, title, list) {
