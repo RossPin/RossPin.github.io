cd7d89ccf9568dd5e7913a30833d75461c7e8b07
RossPin <dorpetz@hotmail.com>
2018-03-01 16:17:14 +1300

    update blogs to use javascript navbar

--- script.js
+++ script.js
@@ -11,18 +11,22 @@ function populateNav (){
   }
   var cultural = document.getElementById('culturalBlogList')
   var technical = document.getElementById('technicalBlogList')
-  addListItem(folder + 'c1-reflection-blog.html', 'Reflection', cultural)
-  addListItem(folder + 'c1-time-and-habits-blog.html', 'Time and Habits', cultural)
-  addListItem(folder + 'c2-emotional-intelligence.html', 'Emotional Intelligence', cultural)
-  addListItem(folder + 'c3-meditation-process.html', 'Meditation Process', cultural)
-  addListItem(folder + 'c4-daily-mindfulness.html', 'Daily Mindfulnes', cultural)
-  addListItem(folder + 't2-html-css-dom-p1.html', 'HTML, CSS and DOM Part 1', technical)
-  addListItem(folder + 't2-html-css-dom-p2.html', 'HTML, CSS and DOM Part Part 2', technical)
-  addListItem(folder + 't3-design-to-web-blog.html', 'Design to Web', technical)
-  addListItem(folder + 't4-javascript-basics.html', 'Technical Blog: JavaScript Basics', technical)
+  addListItem(folder, 'c1-reflection-blog.html', 'Reflection', cultural)
+  addListItem(folder, 'c1-time-and-habits-blog.html', 'Time and Habits', cultural)
+  addListItem(folder, 'c2-emotional-intelligence.html', 'Emotional Intelligence', cultural)
+  addListItem(folder, 'c3-meditation-process.html', 'Meditation Process', cultural)
+  addListItem(folder, 'c4-daily-mindfulness.html', 'Daily Mindfulnes', cultural)
+  addListItem(folder, 't2-html-css-dom-p1.html', 'HTML, CSS and DOM Part 1', technical)
+  addListItem(folder, 't2-html-css-dom-p2.html', 'HTML, CSS and DOM Part Part 2', technical)
+  addListItem(folder, 't3-design-to-web-blog.html', 'Design to Web', technical)
+  addListItem(folder, 't4-javascript-basics.html', 'Technical Blog: JavaScript Basics', technical)
 }
 
-function addListItem (src, title, list) {
-  list.innerHTML += '<li><a href="' + src + '">' + title + '</a></li>'
-
+function addListItem (folder, file, title, list) {
+  if (file == location.href.split("/").slice(-1)){
+    list.innerHTML += '<li class="selected">' + title + '</li>'
+  }
+  else {
+    list.innerHTML += '<li><a href="' + folder + file + '">' + title + '</a></li>'
+  }
 }
