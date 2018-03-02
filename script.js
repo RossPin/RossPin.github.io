3e5002b57abfef7246a971b82c0c4b8229f58a8d
RossPin <dorpetz@hotmail.com>
2018-03-02 14:11:35 +1300

    update navbar to include self-confidence blog

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ function populateCultural(folder, list) {
   addListItem(folder, 'c1-time-and-habits-blog.html', 'Time and Habits', culturalList);
   addListItem(folder, 'c2-emotional-intelligence.html', 'Emotional Intelligence', culturalList);
   addListItem(folder, 'c3-meditation-process.html', 'Meditation Process', culturalList);
-  addListItem(folder, 'c4-daily-mindfulness.html', 'Daily Mindfulnes', culturalList);  
+  addListItem(folder, 'c4-daily-mindfulness.html', 'Daily Mindfulnes', culturalList); 
+  addListItem(folder, 'c5-self-confidence.html', 'Self-Confidence', culturalList); 
 }
 
 function populateTech(folder, list) {  
@@ -30,7 +31,7 @@ function populateTech(folder, list) {
   addListItem(folder, 't2-html-css-dom-p1.html', 'HTML, CSS and DOM Part 1', technicalList)
   addListItem(folder, 't2-html-css-dom-p2.html', 'HTML, CSS and DOM Part Part 2', technicalList)
   addListItem(folder, 't3-design-to-web-blog.html', 'Design to Web', technicalList)
-  addListItem(folder, 't4-javascript-basics.html', 'Technical Blog: JavaScript Basics', technicalList)  
+  addListItem(folder, 't4-javascript-basics.html', 'JavaScript Basics', technicalList)  
 }
 
 function addListItem (folder, file, title, list) {
